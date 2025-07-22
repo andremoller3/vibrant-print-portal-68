267e1d56b14f3b88f6fd67214c6109c9dfd111fa
andremoller3 <95064669+andremoller3@users.noreply.github.com>
2025-07-22 13:49:11 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
@@ -12,10 +11,9 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
-  base: '/vibrant-print-portal-68/',
+  base: './', // <- ISSO É ESSENCIAL PARA GITHUB PAGES COM DOMÍNIO
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
